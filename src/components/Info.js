457781b272cb72ce4c61bae482d6c01992b1e159
RitikaRege31 <138657543+RitikaRege31@users.noreply.github.com>
2023-07-19 18:46:08 +0530

    Add files via upload

---
+++ src/components/Info.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "../Css/Info.css";
+
+function Info(props) {
+  const check = props.adult ? "Yes" : "No";
+
+  return (
+    <div className="info_container">
+      <div className="info">
+        <img src={`https://image.tmdb.org/t/p/w500/${props.image}`} alt="" />
+        <div className="info_text">
+          <h1>{props.title}</h1>
+          <p>{props.overview}</p>
+          <p>Release Date :{props.release}</p>
+          <p>Original Language :{props.language}</p>
+          <p>Adult : {check}</p>
+          <p>Average Vote :{props.vote}</p>
+          <img
+            className="info_img"
+            src="https://img.icons8.com/windows/32/000000/macos-close.png"
+            alt=""
+            onClick={props.enabling}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Info;
---
+++ src/components/Loader.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../Css/Loader.css";
+
+function Loader() {
+  return (
+    <div className="main_loader">
+      <div class="txt" contenteditable="true">
+        DROIDFLIX
+      </div>
+    </div>
+  );
+}
+
+export default Loader;
